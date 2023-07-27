cb977f9f9e6548be812e0468a3d9ba731bdb5438
hanxie <hanxie.wq@alibaba-inc.com>
2023-07-27 11:14:32 +0800

    fix: invoke nas create folder before upload

--- src/code/source-code/download-model2nas/index.js
+++ src/code/source-code/download-model2nas/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const downloads = require('@serverless-devs/downloads').default;
 
 
@@ -9,7 +9,15 @@ exports.handler = async (_event, _context, callback) => {
     const fileUrl = `https://serverless-ai-models-${region}.oss-${region}-internal.aliyuncs.com/chatglm2-6b-int4/pytorch_model.bin`;
     const filename = path.basename(fileUrl);
     const downloadDir = '/mnt/auto/llm/' + process.env.modelPath;
+    if(!fs.ensureDirSync(downloadDir)) {
+        fs.mkdirSync(downloadDir);
+    }
     const modelFile = path.join(downloadDir, filename);
+
+    if(process.env.modelPath !== 'chatglm2-6b-int4') {
+        callback(null, 'do not need to download models');
+        return;
+    }
     if (fs.existsSync(modelFile)) {
         callback(null, 'fiExist');
     } else {
