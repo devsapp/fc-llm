dc2e39e8cd3f0fb2a6613612630124b9fcf61fd1
hanxie <hanxie.wq@alibaba-inc.com>
2023-08-21 11:42:50 +0800

    fix: update download region

--- src/code/embedding-service/source-code/download-model2nas/index.js
+++ src/code/embedding-service/source-code/download-model2nas/index.js
@@ -6,7 +6,7 @@ const downloads = require('@serverless-devs/downloads').default;
 exports.handler = async (_event, _context, callback) => {
   const region = process.env.region || 'cn-hangzhou';
   const embedding_model_name = process.env.EMBEDDING_MODEL_NAME || 'bge-large-zh' // text2vec-large-chinese
-  const fileUrl = `https://serverless-ai-models-${region}.oss-cn-hangzhou-internal.aliyuncs.com/${embedding_model_name}/pytorch_model.bin`;
+  const fileUrl = `https://serverless-ai-models-${region}.oss-${region}-internal.aliyuncs.com/${embedding_model_name}/pytorch_model.bin`;
   const filename = path.basename(fileUrl);
   const downloadDir = `/mnt/auto/embedding/${embedding_model_name}`;
   if (!fs.existsSync(downloadDir)) {
